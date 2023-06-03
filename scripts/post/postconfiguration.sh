01bbbb089a6e8aece2e512f226199ea0515881af
odintdh <odintdh@users.noreply.github.com>
2023-06-03 16:06:06 +0200

    initial version

---
+++ scripts/post/postconfiguration.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+cd /tmp/
+pip install PQMusic
+git clone https://github.com/berarma/oversteer.git
+cd oversteer
+meson build
+cd build
+ninja install
+udevadm control --reload-rules && udevadm trigger
+rm -rf /tmp/oversteer
+sed -i 's/#DefaultTimeoutStopSec.*/DefaultTimeoutStopSec=15s/' /etc/systemd/user.conf
+sed -i 's/#DefaultTimeoutStopSec.*/DefaultTimeoutStopSec=15s/' /etc/systemd/system.conf
+systemctl unmask dconf-update.service
+systemctl enable dconf-update.service
